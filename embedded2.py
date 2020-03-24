d49d0df3429db11a8222cfe81b44296ec0af4624
siarkszy <57746735+siarkszy@users.noreply.github.com>
2020-03-24 14:17:38 +0100

    Update embedded2.py

--- embedded2.py
+++ embedded2.py
@@ -1,10 +1,5 @@
 import urllib, urllib2, cookielib
 
-username = 'admin'
-password = 'mypassword'
 
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-login_data = urllib.urlencode({'username' : username, 'j_password' : password})
 opener.open('http://tieto.com/supersecret/login.php', login_data)
 print resp.read()
